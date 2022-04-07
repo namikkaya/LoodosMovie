96de2546da07772aaa6bff089b1f9adba7545746
namikkaya <namikkaya86@gmail.com>
2022-04-07 09:53:31 +0300

    Loodos-1 Açıklama: Projenin oluşturulması

---
+++ LoodosMovie/LoodosMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosMovie
+//
+//  Created by namik kaya on 7.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
