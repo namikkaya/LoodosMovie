d17d82b2e14d4652ee1df010a6ee7db7208b2228
namikkaya <namikkaya86@gmail.com>
2022-04-07 19:15:49 +0300

    LOODOS-5 - Kullanılması öngörülen kütüphanelerin eklenmesi Açıklama: Alamofire eklendi.

---
+++ LoodosMovie/Pods/Target Support Files/Pods-LoodosMovie-LoodosMovieUITests/Pods-LoodosMovie-LoodosMovieUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoodosMovie_LoodosMovieUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LoodosMovie_LoodosMovieUITests
+@end
---
+++ LoodosMovie/Pods/Target Support Files/Pods-LoodosMovieTests/Pods-LoodosMovieTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoodosMovieTests : NSObject
+@end
+@implementation PodsDummy_Pods_LoodosMovieTests
+@end
