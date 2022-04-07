d919cc82ac0a8971f3d51deab8d6a16bbba2976c
namikkaya <namikkaya86@gmail.com>
2022-04-07 20:09:34 +0300

    LOODOS-5 - Kullanılması öngörülen kütüphanelerin eklenmesi Açıklama: RemoteConfig ve ReachabilitySwift kütüphaneleri eklendi

--- LoodosMovie/LoodosMovie/AppDelegate.swift
+++ LoodosMovie/LoodosMovie/AppDelegate.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseRemoteConfig
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-
+    var remoteConfig: RemoteConfig?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        FirebaseApp.configure()
+        startRemoteConfig()
         return true
+    }
+}
+
+extension AppDelegate {
+    private func startRemoteConfig() {
+        remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig?.configSettings = settings
+        fetchConfig()
+    }
+    
+    private func fetchConfig() {
+        remoteConfig?.fetch { (status, error) -> Void in
+          if status == .success {
+            print("Config fetched!")
+            self.remoteConfig?.activate { changed, error in
+              // data
+            }
+          } else {
+            print("Config not fetched")
+            print("Error: \(error?.localizedDescription ?? "No error available.")")
+          }
+          //self.displayWelcome()
+            // hareket
+        }
     }
 }
 
