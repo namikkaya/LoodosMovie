a68ad8cdb7ed6d0eb1e7a1f5d3f7e366fb6ed1c8
namikkaya <namikkaya86@gmail.com>
2022-04-11 22:19:09 +0300

    LOODOS-8 navigationBar transparent olması +Navigationbar transparan olarak düzenlendi.

--- LoodosMovie/LoodosMovie/Utilities/Base/BaseNavigationController.swift
+++ LoodosMovie/LoodosMovie/Utilities/Base/BaseNavigationController.swift
@@ -11,7 +11,16 @@ class BaseNavigationController: UINavigationController {
     weak var coordinatorDelegate: CommonControllerToCoordinatorDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationBar.isTranslucent = true
         
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            appearance.shadowImage = UIImage()
+            appearance.backgroundColor = .clear
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
     }
-
 }
